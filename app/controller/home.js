15cf85909afa5f78b184ef15febad3699edc0cf8
hardy <yincc1@yonyou.com>
2018-08-29 10:58:36 +0800

    jssdk

--- app/controller/home.js
+++ app/controller/home.js
@@ -3,10 +3,7 @@
 module.exports = app => {
   class HomeController extends app.Controller {
     async index(ctx, next) {
-
-      ctx.logger.info(app.config);
-
-      this.ctx.body = app.config;
+      this.ctx.body = '服务正常启动';
     }
   }
   return HomeController;
